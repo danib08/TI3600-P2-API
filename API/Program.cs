4d69a6ccb24f6e1c734ee5c3a20f6f9b296b2e7f
gabogh123 <gabogh99@gmail.com>
2022-10-13 22:48:48 -0600

    feat(Connection): Add Connection and CORS

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Neo4jClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +9,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "myCors",
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                      });
+});
+
+var client = new BoltGraphClient(new Uri("bolt://localhost:7687"), "neo4j", "1234");
+await client.ConnectAsync();
+builder.Services.AddSingleton<IGraphClient>(client);
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
